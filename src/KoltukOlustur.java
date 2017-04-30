c81da047ee81de21b9495c1231c119320c70cc04
mekhti11 <mekhti11@gmail.com>
2017-04-30 23:58:57 +0300

    Random Koltuk Olusturuldu

--- src/KoltukOlustur.java
+++ src/KoltukOlustur.java
@@ -1,22 +1,56 @@
+/**
+ * 
+ * 
+ * @author mekhti
+ */
+
+
+
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
+import javax.swing.*;
 
 public class KoltukOlustur{
-  private Connection conn;
-  private Statement statement; 
-  
-  public static void main(String[] args) {
-    ConnectMySql cMySql = new ConnectMySql();
-    String sql;
-    conn = cMySql.ConnectDB();
+  public static Connection connection;
+  public static Statement statement; 
+
+    public static Connection ConnectDB(){
     try{
 
-      
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/Cinema","root","mekhti");
+      //JOptionPane.showMessageDialog(null, "Connected to Database");
+      return connect;
+
+    }catch(Exception e){
+      JOptionPane.showMessageDialog(null,e);
+      return null;
+    }
+  }
+  
+  public static void main(String[] args) {
+    ConnectMySql conn = new ConnectMySql();
+    connection = ConnectDB();
+    Random rand = new Random();
+    String sql;
+    int rndm;
+    try{
+      for(int i=1;i<=21;i++)
+        for(int j=1;j<=5;j++)
+          for(int k=1;k<=9;k++){
+            rndm =rand.nextInt(3);
+              System.out.println(rndm);
+            sql = "INSERT INTO `Koltuk`(`GosterimId`, `row`, `sutun`, `value`) VALUES ("+i+","+j+","+k+","+rndm+")";
+            statement = connection.createStatement();   
+            statement.execute(sql);
+          }       
       
     }catch(Exception e){
       System.out.println(e);
     }
 
   }
+  
 
 }
